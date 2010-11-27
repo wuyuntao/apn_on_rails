0b73870a802e6fc5f87e3f886ff6c65b4e5d64d1
Patrick B. Gibson <patrick@fadeover.org>
2010-11-26 23:48:41 -0800

    Fix generator to work with Rails 3.

--- generators/apn_migrations_generator.rb
+++ generators/apn_migrations_generator.rb
@@ -1,31 +1,37 @@
-require 'rails_generator'
+require 'rails/generators/active_record'
+
 # Generates the migrations necessary for APN on Rails.
 # This should be run upon install and upgrade of the 
 # APN on Rails gem.
 # 
-#   $ ruby script/generate apn_migrations
-class ApnMigrationsGenerator < Rails::Generator::Base
+#   $ rails g apn_migrations
+
+class ApnMigrationsGenerator < Rails::Generators::Base
+  argument :name, :default => "migration"
+  @timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
+  include Rails::Generators::Migration
   
-  def manifest # :nodoc:
-    record do |m|
-      timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
-      db_migrate_path = File.join('db', 'migrate')
-      
-      m.directory(db_migrate_path)
-      
-      Dir.glob(File.join(File.dirname(__FILE__), 'templates', 'apn_migrations', '*.rb')).sort.each_with_index do |f, i|
-        f = File.basename(f)
-        f.match(/\d+\_(.+)/)
-        timestamp = timestamp.succ
-        if Dir.glob(File.join(db_migrate_path, "*_#{$1}")).empty?
-          m.file(File.join('apn_migrations', f), 
-                 File.join(db_migrate_path, "#{timestamp}_#{$1}"), 
-                 {:collision => :skip})
-        end
-      end
-      
-    end # record
-    
-  end # manifest
+  def self.source_root
+    @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+  end
   
-end # ApnMigrationsGenerator
+  def self.next_migration_number(dirname)
+    @timestamp = @timestamp.succ
+    @timestamp
+  end
+  
+  def create_migrations
+    migration_template '001_create_apn_devices.rb', 'db/migrate/001_create_apn_devices.rb'
+    migration_template '002_create_apn_notifications.rb', 'db/migrate/002_create_apn_notifications.rb'
+    migration_template '003_alter_apn_devices.rb', 'db/migrate/003_alter_apn_devices.rb'
+    migration_template '004_create_apn_apps.rb', 'db/migrate/004_create_apn_apps.rb'
+    migration_template '005_create_groups.rb', 'db/migrate/005_create_groups.rb'
+    migration_template '006_alter_apn_groups.rb', 'db/migrate/006_alter_apn_groups.rb'
+    migration_template '007_create_device_groups.rb', 'db/migrate/007_create_device_groups.rb'
+    migration_template '008_create_apn_group_notifications.rb', 'db/migrate/008_create_apn_group_notifications.rb'
+    migration_template '009_create_pull_notifications.rb', 'db/migrate/009_create_pull_notifications.rb'
+    migration_template '010_alter_apn_notifications.rb', 'db/migrate/010_alter_apn_notifications.rb'
+    migration_template '011_make_device_token_index_nonunique.rb', 'db/migrate/011_make_device_token_index_nonunique.rb'
+  end
+  
+end
